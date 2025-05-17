bde2f6afa7ca56b94eef77bc0a163ac4e6fa2a36
danie <danie@DANIEL>
2025-05-17 18:37:35 -0500

    Agregar archivos de proyecto.

---
+++ SierraHerreraVargasChatBot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SierraHerreraVargasChatBot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
