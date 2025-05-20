0dae82bf9efbe508ed7df3d227534d11d4062972
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2025-05-20 01:27:14 -0500

    Primer commit de prueba para confirmar cambios

---
+++ SierraHerreraVargasChatBot/Models/ChatBotViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SierraHerreraVargasChatBot.Models
+{
+    public class ChatBotViewModel
+    {
+        public string Prompt { get; set; }
+        public string GeminiResponse { get; set; }
+        public string OpenAIResponse { get; set; }
+    }
+}
---
+++ SierraHerreraVargasChatBot/Models/GeminiModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SierraHerreraVargasChatBot.Models
+{
+    public class GeminiModels
+    {
+        public class GeminiRequest
+        {
+            public List<GeminiContent> contents { get; set; }
+        }
+        public class GeminiContent
+        {
+            public List<GeminiPart> parts { get; set; }
+        }
+        public class GeminiPart
+        {
+            public string text { get; set; }
+        }
+    }
+}
